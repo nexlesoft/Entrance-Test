ccabbf3bec18621518b5a3f8fe3c42d172e3940c
antran@nexlesoft.com <antran@nexlesoft.com>
2022-05-06 15:57:32 +0700

    add loading component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { Grid } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+
 const PhotoGallery = () => {
-	const [data, setData] = useState([]);
-
-	const [page, setPage] = useState(0);
-
 	const API_URL = 'https://www.pinkvilla.com/photo-gallery-feed-page';
 	const DOMAIN_NAME = 'https://www.pinkvilla.com/';
+
+	const [data, setData] = useState([]);
+	const [page, setPage] = useState(0);
+	const [loading, setLoading] = useState(false);
+
 	const fetchData = async () => {
+		setLoading(true);
+
 		try {
 			const { data: response } = await axios.get(`${API_URL}/page/${page}`);
 			const newData = data.concat(response.nodes);
@@ -25,7 +28,9 @@ const PhotoGallery = () => {
 		} catch (error) {
 			console.error(error.message);
 		}
+		setLoading(false);
 	};
+
 	useEffect(() => {
 		fetchData();
 	}, [page]);
@@ -36,7 +41,6 @@ const PhotoGallery = () => {
 				// inverse
 				dataLength={data.length}
 				next={() => {
-					console.log('herreee');
 					setPage(page + 1);
 				}}
 				hasMore={true}
@@ -45,56 +49,52 @@ const PhotoGallery = () => {
 			>
 				{data?.map((item) => (
 					<div style={{ display: 'flex', justifyContent: 'center' }}>
-						<Card
-							sx={{
-								display: 'flex',
-								width: '50%',
-								height: 200,
-								alignItems: 'center',
-							}}
-						>
-							<Grid container>
-								<Grid
-									xs={4}
-									sx={{
-										display: 'flex',
-										justifyContent: 'center',
-										alignItems: 'center',
-									}}
-								>
-									<CardMedia
-										component="img"
-										sx={{ width: 250, height: 180, borderRadius: 10 }}
-										image={`${DOMAIN_NAME}/${item.node.field_photo_image_section}`}
-										alt="Live from space album cover"
-									/>
-								</Grid>
-								<Grid xs={8}>
-									<Box sx={{ display: 'flex', flexDirection: 'column' }}>
-										<CardContent sx={{ flex: '1 0 auto' }}>
-											<Typography component="div" variant="h5">
-												{item.node.title}
-											</Typography>
-											<Typography
-												variant="subtitle1"
-												color="text.secondary"
-												component="div"
-											></Typography>
-										</CardContent>
-										<Box
-											sx={{
-												display: 'flex',
-												alignItems: 'center',
-												pl: 1,
-												pb: 1,
-											}}
-										></Box>
-									</Box>
-								</Grid>
+						<Grid container sx={{ width: '40%', height: 200 }}>
+							<Grid
+								xs={4}
+								sx={{
+									display: 'flex',
+									justifyContent: 'center',
+									alignItems: 'center',
+								}}
+							>
+								<CardMedia
+									component="img"
+									sx={{ width: 250, height: 180, borderRadius: 10 }}
+									image={`${DOMAIN_NAME}/${item.node.field_photo_image_section}`}
+									alt="Live from space album cover"
+								/>
 							</Grid>
-						</Card>
+							<Grid xs={8}>
+								<Box sx={{ display: 'flex', flexDirection: 'column' }}>
+									<CardContent sx={{ flex: '1 0 auto' }}>
+										<Typography component="div" variant="h5">
+											{item.node.title}
+										</Typography>
+										<Typography
+											variant="subtitle1"
+											color="text.secondary"
+											component="div"
+										></Typography>
+									</CardContent>
+									<Box
+										sx={{
+											display: 'flex',
+											alignItems: 'center',
+											pl: 1,
+											pb: 1,
+										}}
+									></Box>
+								</Box>
+							</Grid>
+						</Grid>
 					</div>
 				))}
+				{loading && (
+					<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+						<CircularProgress />
+					</Box>
+				)}
 			</InfiniteScroll>
 		</div>
 	);
